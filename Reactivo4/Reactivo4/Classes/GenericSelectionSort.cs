aeeb717ba81198554f1f3c9546df55609b5bfa6a
Juan Carlos Gómez Abundis <juancg4@live.com>
2019-05-16 12:14:11 -0600

    El commit final con los dos reactivos ahora sí

---
+++ Reactivo4/Reactivo4/Classes/GenericSelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactivo4.Classes
+{
+    class GenericSelectionSort
+    {
+        public static void SelectionSort<T>(T[] array) where T:IComparable
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                T min = array[i];
+                int minId = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(min) < 0)
+                    {
+                        min = array[j];
+                        minId = j;
+                    }
+                }
+                // swapping
+                T temp = array[i];
+                array[i] = min;
+                array[minId] = temp;
+            }
+        }
+    }
+}
---
+++ Reactivo4/Reactivo4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactivo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esto es para probarlo
+            Console.WriteLine("Output del Selection Sort normal:");
+
+            int[] arr2 = { 800, 1, 5, 71, 49, -10, 40, -29 };
+
+            Classes.SelectionSort.selectionSort(arr2);
+
+            for (int i = 0; i < arr2.Length; i++)
+                Console.Write(arr2[i] + "\n");
+
+            Console.WriteLine("\nOutput del Selection Sort con generics:");
+
+            int[] arr = { 800, 1, 5, 71, 49, -10, 40, -29 };
+
+            Classes.GenericSelectionSort.SelectionSort(arr);
+            
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
